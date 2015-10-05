5d53fd7ad713226247a3cc3026c59ce8671ac1fa
andreibosco <andreibosco@gmail.com>
2015-10-04 22:35:56 -0300

    adicionando código dos botões para tirar foto e selecionar foto da library

--- SAC/ViewController.swift
+++ SAC/ViewController.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imagePicker: UIImagePickerController!
+    var newMedia: Bool?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+//    @IBAction func useCamera(sender: UIBarButtonItem) {
+//        imagePicker = UIImagePickerController()
+//        imagePicker.delegate = self
+//        imagePicker.sourceType = .Camera
+//        
+//        presentViewController(imagePicker, animated: true, completion: nil)
+//    }
+    
+//    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+//        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+//        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+//    }
+    
+    @IBAction func useCameraRoll() {
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        picker.sourceType = .PhotoLibrary
+        
+        presentViewController(picker, animated: true, completion: nil)
+    }
+    
+    @IBAction func useCamera() {
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        picker.sourceType = .Camera
+        
+        presentViewController(picker, animated: true, completion: nil)
+    }
 
 }
 
