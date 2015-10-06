d969bb21166c6a4373737688509407894812abc0
andreibosco <andreibosco@gmail.com>
2015-10-05 22:55:35 -0300

    adicionando bridging-header para comunicação com código ObjectiveC

---
+++ SAC/SAC-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  SAC-Bridging-Header.h
+//  SAC
+//
+//  Created by Andrei Bosco on 05/10/15.
+//  Copyright © 2015 Andrei Bosco. All rights reserved.
+//
+
+#ifndef SAC_Bridging_Header_h
+#define SAC_Bridging_Header_h
+
+#import <GooglePlus/GooglePlus.h>
+#import <GooglePlusShareActivity/GPPShareActivity.h>
+
+#endif /* SAC_Bridging_Header_h */
